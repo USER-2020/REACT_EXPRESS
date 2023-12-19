b7fa702a9e784e5df9e14637446335aca40add4d
USER-2020 <hackingetico2021@gmail.com>
2023-12-19 17:49:06 -0500

    style: :zap: btnNavigation, transitions

--- src/containers/BANNER/banner.tsx
+++ src/containers/BANNER/banner.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Bannerimg from '../../components/BANNERIMG/bannerimg'
 import Bannertext from '../../components/BANNERTEXT/bannertext'
 import './banner.css'
+import { useInView } from 'react-intersection-observer'
 
-const Banner = () => {
+const Banner = ({ prevSection }) => {
+
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  useEffect(() => {
+    console.log("Desde el: ", prevSection);
+    // if(prevSection  "banner"){
+    //   setShowAnimation(true);
+    // }
+  }, [])
   return (
     <div className='banner'>
-      <Bannerimg/>
-      <Bannertext/>
+      <Bannerimg showanimation={showAnimation} />
+      <Bannertext />
     </div>
   )
 }
