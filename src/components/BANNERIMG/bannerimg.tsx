4f7f2368b4eca0255699082445997d8af579672f
UNDERSAAR <alesalas1934@hotmail.com>
2023-12-19 17:43:30 -0500

    "NF": Banner almost done, best effect ever

--- src/components/BANNERIMG/bannerimg.tsx
+++ src/components/BANNERIMG/bannerimg.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './bannerimg.css';
-
+import img1 from '../../assets/imgs/bannerimg/S1.jpg'
+import img2 from '../../assets/imgs/bannerimg/S2.jpg'
 
 const Bannerimg = () => {
   return (
     <div className='bannerI'>
-        BannerIMG
+        <img src={img1} alt="" />
+        <img src={img2} alt="" />
     </div>
   )
 }
