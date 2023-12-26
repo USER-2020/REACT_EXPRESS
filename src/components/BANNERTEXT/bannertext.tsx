42ae04862de0ca81b27bb62c482516b3b89043a8
UNDERSAAR <alesalas1934@hotmail.com>
2023-12-25 23:06:10 -0500

    "NF": Banner color

--- src/components/BANNERTEXT/bannertext.tsx
+++ src/components/BANNERTEXT/bannertext.tsx
@@ -1,22 +1,7 @@
 import React from "react";
 import "./bannertext.css";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 const Bannertext = () => {
-  const [color, setColor] = React.useState("");
-
-  const handleChangeC = (event: SelectChangeEvent) => {
-    setColor(event.target.value);
-  };
-
-  const [size, setSize] = React.useState("");
-
-  const handleChangeS = (event: SelectChangeEvent) => {
-    setSize(event.target.value);
-  };
 
   return (
     <div className="bannerT">
@@ -32,42 +17,6 @@ const Bannertext = () => {
         <div className="textP">
           <h2>DE LA MONTAÑA</h2>
           <h1>FUKYU - Nada Personal (Sweater)</h1>
-          <div className="formP">
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel className='inputT' id="demo-simple-select-standard-label">
-              Color
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={color}
-              onChange={handleChangeC}
-              label="color"
-            >
-              <MenuItem value={1} >DORÉ Beau</MenuItem>
-              <MenuItem value={2} >BLEU Amour</MenuItem>
-              <MenuItem value={3} >ROUGE Neuviéme</MenuItem>
-              <MenuItem value={4} >BLANC Montagne</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel className='inputT' id="demo-simple-select-standard-label">
-              Talla
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={size}
-              onChange={handleChangeS}
-              label="size"
-            >
-              <MenuItem value={5} >S</MenuItem>
-              <MenuItem value={6} >M</MenuItem>
-              <MenuItem value={7} >L</MenuItem>
-              <MenuItem value={8} >XL</MenuItem>
-            </Select>
-          </FormControl>
-          </div>
         </div>
         <div className="buyN"></div>
       </div>
