f17ab5b68335e635ca9a153e096690cbfad4e473
UNDERSAAR <alesalas1934@hotmail.com>
2023-12-25 22:51:29 -0500

    "NF": Ni idea

--- src/components/PRELOADER/preloader.tsx
+++ src/components/PRELOADER/preloader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./preloader.css";
-import logob from "../../assets/imgs/logo/Noble.png";
+import logob from "../../assets/imgs/logo/Noble2.png";
 
 const Preloader = () => {
   return (
