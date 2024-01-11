3e966b484657c23d23859b108e69fc51a65c19fc
USER - 2020 <hackingetico2021@gmail.com>
2024-01-11 17:19:32 -0500

    feat: :zap: ADDD NEW FEATURES: server cconnected success and post imagen and products

--- src/backend/containsDynamic/productos/modalAddProductos/modalAddProducts.tsx
+++ src/backend/containsDynamic/productos/modalAddProductos/modalAddProducts.tsx
@@ -2,34 +2,241 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Fade from '@mui/material/Fade'
 import Typography from '@mui/material/Typography'
-import React from 'react'
+import FormGroup from '@mui/material/FormGroup';
+import TextField from '@mui/material/TextField';
+import React, { useState } from 'react'
+import { addProducts } from '../../../../services/productos/productos';
+import Avatar from '@mui/material/Avatar';
+import Input from '@mui/material/Input';
+import './modalAddProducts.css';
+import { categories } from '../../../data/categoriesData';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import { tallas } from '../../../data/tallasData';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import { Form } from 'reactstrap';
 
 const ModalAddProducts = ({ handleClose, valueModalVisible }) => {
+    const [productData, setProductData] = useState({
+        nombre: '',
+        categoria: '',
+        precio: '',
+        stock: '',
+        coleccion: '',
+        talla: '',
+        color: '',
+        tipo_prenda: '',
+        imagenRuta: '',
+    });
+    const [imagenPreview, setImagenPreview] = useState(null);
+    const [file, setFile] = useState(null);
+    // const [file, setFile] = useState(null);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setProductData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+
+        if (file) {
+            const fileName = `images/imgsProducts/${file.name}`;
+
+            // Crear nuevo objeto FormData
+            const formData = new FormData();
+
+            // Agregar la imagen al objeto FormData
+            formData.append('imagenRuta', file);
+
+            // Actualizar el estado con el nombre del archivo
+            setProductData((prevData) => ({
+                ...prevData,
+                imagenRuta: fileName,
+            }));
+
+            // Actualizar el estado para mostrar la previsualización temporal
+            setImagenPreview(URL.createObjectURL(file));
+            // Almacena el archivo en el estado
+            setFile(file);
+
+            // Imprimir formData en la consola para verificar
+            console.log("formData con imagen:", formData);
+        }
+    };
+
+
+    // const handleFileChange = (e) => {
+
+    //     handleImageChange(e);
+    // };
+
+    const handleSubmit = () => {
+        const formData = new FormData();
+
+        // Agregar todos los campos al objeto FormData
+        formData.append('nombre', productData.nombre);
+        formData.append('categoria', productData.categoria);
+        formData.append('precio', productData.precio);
+        formData.append('stock', productData.stock);
+        formData.append('coleccion', productData.coleccion);
+        formData.append('talla', productData.talla);
+        formData.append('color', productData.color);
+        formData.append('tipo_prenda', productData.tipo_prenda);
+
+        // Verificar si hay una imagen antes de agregarla
+        if (file) {
+            console.log(file);
+            formData.append('imagen', file);
+        }
+
+        console.log(formData);
+        // Enviar formData al servidor
+        addProducts(formData)
+            .then((res) => {
+                console.log(res);
+                handleClose();
+            })
+            .catch((err) => console.log(err));
+    };
+
+
     return (
         <Fade in={valueModalVisible}>
-            <Box
-                sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: 920,
-                    bgcolor: 'background.paper',
-                    border: 'none',
-                    borderRadius: 8,
-                    boxShadow: 24,
-                    p: 4,
-                }}
-            >
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Ingresa aqui tu producto
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Contenido del modal. Puedes poner lo que quieras aquí.
-                </Typography>
-                <Button onClick={handleClose} sx={{ mt: 2 }}>
-                    Cerrar modal
-                </Button>
+            <Box sx={{ padding: '20px' }}>
+                <Form onSubmit={handleSubmit} encType="multipart/form-data">
+                    <FormGroup sx={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-between' }}>
+                        <TextField
+                            label="Nombre"
+                            name="nombre"
+                            value={productData.nombre}
+                            onChange={handleChange}
+
+                            sx={{ mt: 2, width: '49%' }}
+                        />
+                        <Select
+                            displayEmpty
+                            value={productData.categoria}
+                            onChange={handleChange}
+                            input={<OutlinedInput />}
+                            name='categoria'
+                            sx={{ mt: 2, width: '49%' }}
+                        >
+                            <MenuItem disabled value="">
+                                <em>Categorias...</em>
+                            </MenuItem>
+                            {categories.map((categoria) => (
+                                <MenuItem
+                                    key={categoria.id}
+                                    value={categoria.name}
+                                >
+                                    {categoria.name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormGroup>
+                    <FormGroup sx={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-between' }}>
+
+                        <TextField
+                            label="Precio"
+                            name="precio"
+                            value={productData.precio}
+                            onChange={handleChange}
+
+                            sx={{ mt: 2, width: '49%' }}
+                        />
+                        <TextField
+                            label="Stock"
+                            name="stock"
+                            value={productData.stock}
+                            onChange={handleChange}
+
+                            sx={{ mt: 2, width: '49%' }}
+                        />
+                    </FormGroup>
+                    <FormGroup sx={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-between' }}>
+                        <TextField
+                            label="Colección"
+                            name="coleccion"
+                            value={productData.coleccion}
+                            onChange={handleChange}
+
+                            sx={{ mt: 2, width: '49%' }}
+                        />
+                        <Select
+                            displayEmpty
+                            value={productData.talla}
+                            onChange={handleChange}
+                            input={<OutlinedInput />}
+                            name='talla'
+                            sx={{ mt: 2, width: '49%' }}
+                        >
+                            <MenuItem disabled value="">
+                                <em>Tallas...</em>
+                            </MenuItem>
+                            {tallas.map((talla) => (
+                                <MenuItem
+                                    key={talla.id}
+                                    value={talla.referencia}
+                                >
+                                    {talla.referencia}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormGroup>
+                    <FormGroup sx={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-between' }}>
+                        <TextField
+                            label="Color"
+                            name="color"
+                            value={productData.color}
+                            onChange={handleChange}
+
+                            sx={{ mt: 2, width: '49%' }}
+                        />
+                        <TextField
+                            label="Tipo de prenda"
+                            name="tipo_prenda"
+                            value={productData.tipo_prenda}
+                            onChange={handleChange}
+
+                            sx={{ mt: 2, width: '49%' }}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Input
+                            // accept="image/*"
+                            id="contained-button-file"
+                            type="file"
+                            name="imagen"
+                            sx={{ display: 'none', mt: 2 }}
+                            onChange={handleFileChange}
+                        />
+
+                        {imagenPreview && (
+                            <Avatar
+                                alt="Previsualización de la imagen"
+                                src={imagenPreview}
+                                sx={{ width: 200, height: 200, ml: 2 }}
+                            />
+                        )}
+                        <label htmlFor="contained-button-file">
+                            <Button variant="contained" component="span" sx={{ mt: 2 }}>
+                                Subir Imagen
+                            </Button>
+                        </label>
+                    </FormGroup>
+
+                    <Button onClick={handleSubmit} sx={{ mt: 2 }}>
+                        Guardar producto
+                    </Button>
+                    <Button onClick={handleClose} sx={{ mt: 2, ml: 2 }}>
+                        Cerrar modal
+                    </Button>
+                </Form>
             </Box>
         </Fade>
     )
--- src/backend/containsDynamic/productos/tableProductosAdmin.tsx
+++ src/backend/containsDynamic/productos/tableProductosAdmin.tsx
@@ -12,8 +12,10 @@ import Pagination from '@mui/material/Pagination'
 import { getAllProducts } from '../../../services/productos/productos'
 import Fab from '@mui/material/Fab'
 import AddIcon from '@mui/icons-material/Add';
-import Modal from '@mui/material/Modal'
 import ModalAddProducts from './modalAddProductos/modalAddProducts'
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+import { Modal, ModalHeader } from 'reactstrap'
 // import { dataStore } from './../../../data/dataStore';
 
 const TableProductosAdmin = () => {
@@ -31,12 +33,15 @@ const TableProductosAdmin = () => {
                 console.log(res);
                 setProductos(res.data.results);
                 setTotalResults(res.data.total);
-                let countPages = (res.data.total) / 10;
+
+                // Calcular el número de páginas y redondear hacia arriba
+                const countPages = Math.ceil(res.data.total / 10);
+
                 setPages(countPages);
                 console.log(res.data.results.length);
-            }).catch((err) => console.log(err));
-
-    }
+            })
+            .catch((err) => console.log(err));
+    };
 
     const handlePaginate = (event, value) => {
         setCurrentPage(value);
@@ -68,6 +73,9 @@ const TableProductosAdmin = () => {
                                 <TableCell>Talla</TableCell>
                                 <TableCell>Color</TableCell>
                                 <TableCell>Tipo prenda</TableCell>
+                                <TableCell>Acciones</TableCell>
+
+
                                 {/* <TableCell align="right">Sale Amount</TableCell> */}
                             </TableRow>
                         </TableHead>
@@ -83,19 +91,31 @@ const TableProductosAdmin = () => {
                                     <TableCell>{row.talla}</TableCell>
                                     <TableCell>{row.color}</TableCell>
                                     <TableCell>{row.tipo_prenda}</TableCell>
+                                    <TableCell>
+                                        <ButtonGroup
+                                            disableElevation
+                                            variant="contained"
+                                            aria-label="Disabled elevation buttons"
+                                        >
+                                            <Button>Editar</Button>
+                                            <Button color='error'>Eliminar</Button>
+                                        </ButtonGroup>
+                                    </TableCell>
                                     {/* <TableCell align="right">{`$${row.amount}`}</TableCell> */}
                                 </TableRow>
                             ))}
                         </TableBody>
                     </Table>
-                    <Pagination count={pages} onChange={handlePaginate} />
+                    {totalResults && totalResults >= 10 ? (
+                        <Pagination count={pages} onChange={handlePaginate} />
+                    ) : (null)}
 
                     {/* <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
                         See more orders
                     </Link> */}
                 </React.Fragment>
             </Paper>
-            <Fab variant="extended"
+            <Fab
                 variant="extended"
                 onClick={handleModalAddProducts}
                 style={{
@@ -106,11 +126,15 @@ const TableProductosAdmin = () => {
                 <AddIcon sx={{ mr: 1, }} />
                 Agregar
             </Fab>
-            <Modal open={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description">
-                <ModalAddProducts handleClose={() => setIsModalOpen(false)} valueModalVisible={isModalOpen}/>
+            <Modal
+                size='lg'
+                style={{ top: 60 }}
+                isOpen={isModalOpen}
+                toggle={() => setIsModalOpen(false)}
+
+            >
+                <ModalHeader toggle={() => setIsModalOpen(false)}>Ingresa aquí tu producto</ModalHeader>
+                <ModalAddProducts handleClose={() => setIsModalOpen(false)} valueModalVisible={isModalOpen} />
             </Modal>
         </div>
 
