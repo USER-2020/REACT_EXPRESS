ac6d8048dfe50da6bace37dde2eebe716b416f7d
USER - 2020 <hackingetico2021@gmail.com>
2024-01-12 16:20:31 -0500

    feat: :zap: new features products

--- src/components/PRODUCT/product.tsx
+++ src/components/PRODUCT/product.tsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import './product.css'
-const Product = () => {
-  return (
-    <div className='prod'>Product
-      
-    
-    </div>
-  )
-}
+import React, { useEffect, useState } from 'react';
+import './product.css';
+import img1 from "../../assets/imgs/bannerimg/S2.jpg";
+import img2 from "../../assets/imgs/bannerimg/S1.jpg";
 
-export default Product
+const Product = ({ product }) => {
+  const [precio, setPrecio] = useState();
+
+  useEffect(() => {
+    // Convierte la cadena a un número (puede ser parseFloat o parseInt según tus necesidades)
+    const precioNumerico = parseFloat(product.precio);
+
+    // Verifica si la conversión fue exitosa y si es un número válido
+    if (!isNaN(precioNumerico)) {
+      const precioFormateado = precioNumerico.toLocaleString('es-ES');
+      console.log(`<h4>${precioFormateado}</h4>`);
+      setPrecio(precioFormateado);
+    } else {
+      console.error("El valor de precio no es un número válido.");
+    }
+  }, [product.precio]);
+
+  const baseUrlImagen = `/public`;
+
+  return (
+    <div className='prod'>
+      <div className="sect">
+        <div className="pimg">
+          <img src={`${baseUrlImagen}/${product.imagenRuta}`} alt={product.imagenRuta} />
+        </div>
+        <div className="ptext">
+          <div className="text1">
+            <h3>{product.nombre}</h3>
+            <h4>{product.coleccion}</h4>
+            <h4>${precio}</h4>
+          </div>
+          <div className="text2">
+            <a href="#">+</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
--- src/containers/PRODUCTS/products.tsx
+++ src/containers/PRODUCTS/products.tsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from '../../components/PRODUCT/product'
 import './products.css'
+import { getAllProducts } from '../../services/productos/productos';
 const Products = () => {
+  const [productos, setProductos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const getProductos = () => {
+    getAllProducts(currentPage, 6)
+      .then((res) => {
+        console.log(res);
+        setProductos(res.data.results);
+        // console.log(res.data.results);
+        // setTotalResults(res.data.total);
+
+        // Calcular el número de páginas y redondear hacia arriba
+        const countPages = Math.ceil(res.data.total / 10);
+
+        // setPages(countPages);
+        console.log(res.data.results.length);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getProductos();
+  }, []);
+
   return (
     <div className="prodS">
-      <Product/>
-      <Product/>
-      <Product/>
-      <Product/>
-      <Product/>
-      <Product/>
+      {productos && productos.length > 0 && productos.map((productoInfo) => (
+        <Product key={productoInfo.id} product={productoInfo} />
+      ))}
+      {/* <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product /> */}
     </div>
+
   )
 }
 
